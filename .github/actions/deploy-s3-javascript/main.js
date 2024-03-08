787af25608c96151d688ea9a046e3cfa95624cf5
APdrone <amanpatwalfor@gmail.com>
2024-03-08 21:21:04 +0530

    adding logs

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -7,7 +7,9 @@ function run() {
   const bucket = core.getInput('bucket', { required: true });
   const bucketRegion = core.getInput('bucket', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
-
+  core.notice(bucket);
+  core.notice(bucketRegion);
+  core.notice(distFolder);
   //  2) upload files
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
